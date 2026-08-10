abc7750dcce28f2cd879b80854505c77afcc8616
GitHub Action <action@github.com>
2026-08-10 02:03:45 +0000

    自動更新 1999 派工 API ID

--- fetch_data.py
+++ fetch_data.py
@@ -8,7 +8,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # 從 App.vue 取得的 API URL
-API_URL = 'https://data.taipei/api/v1/dataset/5b68c8f3-59f8-402d-adb3-1105b0b248f4?scope=resourceAquire'
+API_URL = 'https://data.taipei/api/v1/dataset/2369db47-8e7e-487c-a198-a93ad63f7e7c?scope=resourceAquire'
 # CKAN API 通常的單次請求上限是 1000
 LIMIT = 1000
 # 輸出路徑，對應 Vue 的 public 資料夾
